f4218de718dea6e76202ce63b677e19bac807671
Melvin Bulan <melvin.bulan000@gmail.com>
2023-03-14 11:43:04 +0800

    Add AboutUsPage and styling

---
+++ src/components/Pages/AboutUsSection/AboutUsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../AboutUsSection/AboutUsPage.module.scss"
+
+const AboutUsPage = () => {
+  return (
+    <div className={styles.container}>
+        <h1>test</h1>
+    </div>
+  )
+}
+
+export default AboutUsPage
