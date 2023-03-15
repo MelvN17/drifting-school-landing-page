18dabbaa73f791bdabcf412faab55e9e7a7f8a44
Melvin Bulan <melvin.bulan000@gmail.com>
2023-03-15 15:09:48 +0800

    Add files and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './App.scss'
 import AboutUsPage from './components/Pages/AboutUsSection/AboutUsPage'
+import GalleryPage from './components/Pages/GallerySection/GalleryPage'
 import HeroPage from './components/Pages/HeroSection/2.MainSection/HeroPage'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <HeroPage />
       <AboutUsPage />
-      <AboutUsPage />
+      <GalleryPage />
     </div>
   )
 }
